3e0fc26c9fe363ffcdf55b09bd0b3af921160c52
smarog <smaro13@windowslive.com>
2024-12-28 12:05:25 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Smaro Goulianou on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
